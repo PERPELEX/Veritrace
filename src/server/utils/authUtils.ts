bfa97c3af4e3418e18c79f123524eff4e11d63f7
AbdulHannanAmir <hannan.amir@infixsystems.com>
2026-04-05 12:43:42 +0500

    Bild Bug Fix

--- src/server/utils/authUtils.ts
+++ src/server/utils/authUtils.ts
@@ -7,7 +7,7 @@ export function generateAccessToken(userId: string, role: string) {
   return jwt.sign(
     { userId, role },
     process.env.ACCESS_TOKEN_SECRET as string,
-    { expiresIn: "15m" }
+    { expiresIn: "60m" }
   );
 }
 
---
+++ src/types/custom.d.ts
@@ -0,0 +1,5 @@
+declare module 'jsonwebtoken';
+declare module 'bcryptjs';
+declare module 'nodemailer';
+declare module 'leaflet';
+declare module 'react-leaflet';
